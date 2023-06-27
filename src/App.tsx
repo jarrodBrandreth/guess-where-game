af2a5615b5aac64129bef362164b61597aaf2137
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-06-27 19:20:58 +0200

    implemented routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './routes/Layout';
+import Home from './routes/Home';
+import Game from './routes/Game';
+import Countries from './routes/Countries';
+import About from './routes/About';
+
 function App() {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="game" element={<Game />} />
+          <Route path="countries" element={<Countries />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
