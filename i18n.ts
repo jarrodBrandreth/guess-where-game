a8b5e8ccad17b20200d528e1fd317840ee0de7fc
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-08-11 16:45:52 +0200

    added country ns

--- i18n.ts
+++ i18n.ts
@@ -28,7 +28,7 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     // add to ns when adding a new namespace json file in locales
-    ns: ['common', 'capitals', 'countries'],
+    ns: ['common', 'capitals', 'countries', 'country'],
     defaultNS: 'common',
     fallbackNS: 'common',
     detection: options,
