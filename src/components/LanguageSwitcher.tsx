96373627d0b440fae862d9df1e98596a0ecce08a
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-08-11 16:53:43 +0200

    re done to use Dropdown component

--- src/components/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher.tsx
@@ -1,17 +1,28 @@
-import { useCallback, useState } from 'react';
-import { useOutsideClick } from '../hooks/useOutsideClick';
-import LanguageButton from './LanguageButton';
-import LanguageOptions from './LanguageOptions';
+import Dropdown from './Dropdown/Dropdown';
+import LanguageIcon from './icons/LanguageIcon';
+import DropdownOption from './Dropdown/DropdownOption';
+import { useLanguageContext } from '../hooks/useLanguageContext';
+import { useTranslation } from 'react-i18next';
 
 export default function LanguageSwitcher() {
-  const [showOptions, setShowOptions] = useState(false);
-  const closeOptions = useCallback(() => setShowOptions(false), []);
-  const { containerRef } = useOutsideClick<HTMLDivElement>(closeOptions);
+  const { currentLanguage, changeLanguage, supportedLanguages } = useLanguageContext();
+  const { t } = useTranslation();
 
   return (
-    <div ref={containerRef} className="relative w-fit leading-none">
-      <LanguageButton showing={showOptions} onClick={() => setShowOptions((prev) => !prev)} />
-      <LanguageOptions showing={showOptions} />
-    </div>
+    <Dropdown
+      buttonStyle="icon"
+      label={t('language')}
+      selected={currentLanguage}
+      Icon={LanguageIcon}
+    >
+      {Object.keys(supportedLanguages).map((lng) => (
+        <DropdownOption
+          key={lng}
+          label={supportedLanguages[lng].nativeName}
+          currentSelected={lng === currentLanguage}
+          onClick={() => changeLanguage(lng)}
+        />
+      ))}
+    </Dropdown>
   );
 }
