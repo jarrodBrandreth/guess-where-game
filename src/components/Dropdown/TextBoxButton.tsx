d16ae922f608b20f1cf90fc9de6f005fefc98b6e
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-08-14 11:04:48 +0200

    min width style

--- src/components/Dropdown/TextBoxButton.tsx
+++ src/components/Dropdown/TextBoxButton.tsx
@@ -15,14 +15,14 @@ export default function TextBoxButton({ label, selected, showing, onClick }: Pro
         {label}:
       </span>
       <button
-        className={`flex items-center justify-start gap-1 rounded-md border-2 border-neutral-300 px-2 py-1 outline-none hover:border-pop focus:border-pop dark:border-neutral-600 dark:hover:border-pop dark:focus:border-pop ${
+        className={`flex min-w-[140px] items-center justify-start gap-1 rounded-md border-2 border-neutral-300 px-2 py-1 outline-none hover:border-pop focus:border-pop dark:border-neutral-600 dark:hover:border-pop dark:focus:border-pop ${
           showing ? 'border-pop' : ''
         }`}
         aria-labelledby="dropdownSelection"
         onClick={onClick}
       >
-        <span>{selected}</span>
-        <span className="flex items-start" aria-hidden>
+        <span className="flex-grow">{selected}</span>
+        <span className="pointer-events-none flex items-center" aria-hidden>
           {showing ? <DropUpIcon /> : <DropDownIcon />}
         </span>
       </button>
