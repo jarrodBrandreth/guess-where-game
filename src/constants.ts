4424a5475d6926176f5bc8c750100b827beb774d
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-08-14 11:08:41 +0200

    created constants file

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+export const countriesFilters = {
+  Americas: ['South America', 'Central America', 'Caribbean', 'North America'],
+  Africa: [
+    'Eastern Africa',
+    'Middle Africa',
+    'Southern Africa',
+    'Western Africa',
+    'Northern Africa',
+  ],
+  Asia: ['Eastern Asia', 'Western Asia', 'Southern Asia', 'South-Eastern Asia', 'Central Asia'],
+  Europe: [
+    'Western Europe',
+    'Central Europe',
+    'Eastern Europe',
+    'Southeast Europe',
+    'Northern Europe',
+    'Southern Europe',
+  ],
+  Oceania: ['Polynesia', 'Micronesia', 'Australia and New Zealand', 'Melanesia'],
+};
