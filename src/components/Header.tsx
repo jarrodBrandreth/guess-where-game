67dec14c2b9016c1e779cf2934ea767cdbb83834
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-08-17 14:29:09 +0200

    add z index to prevent overlap

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import LanguageSwitcher from './LanguageSwitcher';
 
 export default function Header() {
   return (
-    <header className="sticky top-0 flex w-screen items-center justify-between bg-neutral-200 px-4 dark:bg-neutral-900 md:border-b md:border-neutral-300 md:bg-neutral-50 md:px-7 dark:md:border-neutral-600 dark:md:dark:bg-neutral-800 lg:justify-end">
+    <header className="sticky top-0 z-40 flex w-screen items-center justify-between bg-neutral-200 px-4 dark:bg-neutral-900 md:border-b md:border-neutral-300 md:bg-neutral-50 md:px-7 dark:md:border-neutral-600 dark:md:bg-neutral-800 lg:justify-end">
       <Logo />
       <div className="flex items-baseline bg-inherit py-5 md:mr-4 md:py-4">
         <Navigation />
