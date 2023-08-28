7e2ef6610c0e55354d9454e4874b584ef8b2c885
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-08-28 11:46:30 +0200

    updated to work with new translation key name

--- src/components/NoResults.tsx
+++ src/components/NoResults.tsx
@@ -11,7 +11,9 @@ export default function NoResults({ searchValue, clearFilters }: Props) {
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-6 p-4">
-      <p className="text-center text-lg font-semibold">{t('noResults', { query: searchValue })}</p>
+      <p className="text-center text-lg font-semibold">
+        {t('noResultsFound', { query: searchValue })}
+      </p>
       <ButtonOutlined content={t('clearFiltersTryAgain')} squared onClick={clearFilters} />
     </div>
   );
