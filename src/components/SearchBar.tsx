1cdfc36365c2419d2a688ff504f26ee6dcbfccfc
Jarrod Brandreth <jarrod.brandreth@gmail.com>
2023-08-28 11:50:17 +0200

    updated to accept optional onFocus and onBlur events and optional Icon change

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,33 +1,49 @@
 import SearchIcon from './icons/SearchIcon';
 import CloseIcon from './icons/CloseIcon';
 import { useTranslation } from 'react-i18next';
+import { SvgIconProps } from '../types';
 
 interface Props {
   searchValue: string;
-  updateSearch: (s: string) => void;
+  updateSearch: (query: string) => void;
   // translated input placeholder
   placeholder: string;
+  // optional icon to change, defaults to search icon if none provided
+  Icon?: ({ className, size }: SvgIconProps) => JSX.Element;
+  onBlur?: () => void;
+  onFocus?: () => void;
 }
 
-export default function SearchBar({ placeholder, searchValue, updateSearch }: Props) {
+export default function SearchBar({
+  placeholder,
+  searchValue,
+  updateSearch,
+  Icon,
+  onBlur,
+  onFocus,
+}: Props) {
   const { t } = useTranslation();
   const nameId = placeholder.replaceAll(' ', '');
+  const SearchBarIcon = Icon ?? SearchIcon;
+
   return (
-    <div className="group relative h-fit w-[min(100%,224px)]">
-      <span className="pointer-events-none absolute top-1/2 -translate-y-1/2 pl-3 opacity-60">
-        <SearchIcon />
+    <div className="group relative h-fit w-full">
+      <span className="pointer-events-none absolute top-1/2 -translate-y-1/2 pl-2 opacity-40 group-focus-within:opacity-80">
+        <SearchBarIcon size="24px" />
       </span>
       <label className="sr-only" htmlFor={nameId}>
         {placeholder}
       </label>
       <input
-        className="w-full overflow-ellipsis rounded-2xl border-2 border-neutral-300 bg-transparent px-9 py-1 caret-pop outline-none group-focus-within:border-pop group-hover:border-pop dark:border-neutral-600 dark:focus:border-pop"
+        className="w-full overflow-ellipsis rounded-md border border-neutral-300 bg-transparent px-9 py-1 caret-green-600 outline-none focus:border-green-700 group-hover:border-green-700 dark:border-neutral-600 dark:caret-green-500 dark:focus:border-green-600 group-hover:dark:border-green-600"
         type="text"
         id={nameId}
         name={nameId}
         placeholder={placeholder}
         value={searchValue}
         onChange={(e) => updateSearch(e.target.value)}
+        onFocus={onFocus}
+        onBlur={onBlur}
       />
       {searchValue && (
         <button
